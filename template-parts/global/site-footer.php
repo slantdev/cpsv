c9b7e10268de5dd6f3a7ae69c5fe90431b68f117
Slant Dev <build@amazeworx.com>
2024-05-14 23:05:49 +0700

    footer

--- template-parts/global/site-footer.php
+++ template-parts/global/site-footer.php
@@ -1,7 +1,35 @@
 <?php
 
 $site_logo = get_stylesheet_directory_uri() . '/assets/images/logo/logo-cpsv.svg';
+$about = get_field('about', 'option');
+$logo = $about['logo'] ?? '';
+$slogan = $about['slogan'] ?? '';
+$address = $about['address'] ?? '';
+$phone = $about['phone'] ?? '';
+$email = $about['email'] ?? '';
 
+$acknowledgement = get_field('acknowledgement', 'option');
+$acknowledgement_flags = $acknowledgement['flags'] ?? ''; // Gallery
+$acknowledgement_text = $acknowledgement['acknowledgement_text'] ?? '';
+
+$quick_links = get_field('quick_links', 'option');
+$quick_links_heading = $quick_links['heading'] ?? '';
+$quick_links_links = $quick_links['links'] ?? ''; // Repeater
+
+$about_us = get_field('about_us', 'option');
+$about_us_heading = $about_us['heading'] ?? '';
+$about_us_links = $about_us['links'] ?? ''; // Repeater
+
+$get_in_touch = get_field('get_in_touch', 'option');
+$get_in_touch_heading = $get_in_touch['heading'] ?? '';
+$get_in_touch_links = $get_in_touch['links'] ?? ''; // Repeater
+
+$copyright_info = get_field('copyright_info', 'option');
+$copyright_site_name = $copyright_info['copyright_site_name'] ?? '';
+$copyright_links = $copyright_info['copyright_links'] ?? '';
+
+$top_navigation = get_field('top_navigation', 'option')['top_navigation'];
+$social_links = $top_navigation['social_links'] ?? '';
 ?>
 
 <!-- Subscribe -->
@@ -61,60 +89,110 @@ $section_id = 'section-subscribe-' . uniqid();
     <div class="container max-w-screen-2xl">
       <div class="flex gap-x-20 items-center pb-12 border-b border-slate-300">
         <div class="w-1/2">
-          <div class="site-logo">
-            <a href="<?php echo site_url() ?>"><img src="<?php echo $site_logo ?>" alt="<?php echo get_bloginfo('name'); ?>" class="h-20 xl:h-[128px] w-auto"></a>
-          </div>
+          <?php if ($logo) : ?>
+            <div class="site-logo">
+              <a href="<?php echo site_url() ?>"><img src="<?php echo $logo['url'] ?>" alt="<?php echo get_bloginfo('name'); ?>" class="h-20 xl:h-[128px] w-auto"></a>
+            </div>
+          <?php endif ?>
         </div>
         <div class="w-1/2">
-          <div class="text-2xl">
-            <strong>75 Years of Unwavering Love</strong> - Ensuring every Cat finds a loving, safe & healthy home.
-          </div>
+          <?php if ($slogan) : ?>
+            <div class="text-2xl">
+              <?php echo $slogan ?>
+            </div>
+          <?php endif ?>
         </div>
       </div>
     </div>
     <div class="container max-w-screen-2xl pt-12">
       <div class="flex gap-x-20">
         <div class="w-1/2 flex flex-col">
-          <div class="leading-loose max-w-[42ch]">
-            We acknowledge Aboriginal and Torres Strait Islander people as the traditional custodians of this land and pay our respects to their history, their living culture and to Elders past and present.
-          </div>
-          <div class="flex gap-x-4 mt-auto">
-            <img src="<?php echo cpsv_asset('/images/content/flag-1.png') ?>" class="h-[48px] w-auto" />
-            <img src="<?php echo cpsv_asset('/images/content/flag-2.png') ?>" class="h-[48px] w-auto" />
-            <img src="<?php echo cpsv_asset('/images/content/flag-3.png') ?>" class="h-[48px] w-auto" />
-          </div>
+          <?php if ($copyright_site_name) : ?>
+          <?php endif; ?>
+          <?php if ($address || $phone || $email) : ?>
+            <div class="flex flex-col gap-y-0.5">
+              <?php if ($address) {
+                echo '<div>Visit: ' . $address . '</div>';
+              } ?>
+              <?php if ($phone) {
+                $phone_link = $phone['url'] ?? '';
+                echo '<div>Phone: <a href="' . $phone_link . '" class="underline hover:no-underline">' . $phone['title'] . '</a></div>';
+              } ?>
+              <?php if ($email) {
+                $email_link = $email['url'] ?? '';
+                echo '<div>Email: <a href="' . $email_link . '" class="underline hover:no-underline">' . $email['title'] . '</a></div>';
+              } ?>
+            </div>
+          <?php endif; ?>
+          <?php if ($acknowledgement_text || $acknowledgement_flags) : ?>
+            <div class="mt-auto">
+              <?php
+              if ($acknowledgement_text) {
+                echo '<div class="max-w-[42ch] text-sm leading-normal mt-8">';
+                echo $acknowledgement_text;
+                echo '</div>';
+              }
+              ?>
+              <?php
+              if ($acknowledgement_flags) {
+                echo '<div class="flex gap-x-4 mt-4">';
+                foreach ($acknowledgement_flags as $flag) {
+                  $flag_url = $flag ?? '#';
+                  echo '<img src="' . $flag . '" class="h-[36px] w-auto" />';
+                }
+                echo '</div>';
+              }
+              ?>
+            </div>
+          <?php endif ?>
         </div>
         <div class="w-1/2">
           <div class="flex gap-x-10">
-            <div class="w-1/3">
-              <h4 class="text-lg font-semibold mb-4">Quick Links</h4>
-              <ul class="flex flex-col gap-y-3">
-                <li><a href="#" class="hover:underline">Adopt a Cat</a></li>
-                <li><a href="#" class="hover:underline">Foster Care</a></li>
-                <li><a href="#" class="hover:underline">Vet Clinic</a></li>
-                <li><a href="#" class="hover:underline">Support us</a></li>
-                <li><a href="#" class="hover:underline">Help & Advice</a></li>
-                <li><a href="#" class="hover:underline">Shop</a></li>
-              </ul>
-            </div>
-            <div class="w-1/3">
-              <h4 class="text-lg font-semibold mb-4">About Us</h4>
-              <ul class="flex flex-col gap-y-3">
-                <li><a href="#" class="hover:underline">Our Vision</a></li>
-                <li><a href="#" class="hover:underline">Our History</a></li>
-                <li><a href="#" class="hover:underline">Meet The Board</a></li>
-                <li><a href="#" class="hover:underline">Annual Reports</a></li>
-              </ul>
-            </div>
-            <div class="w-1/3">
-              <h4 class="text-lg font-semibold mb-4">Get in Touch</h4>
-              <ul class="flex flex-col gap-y-3">
-                <li><a href="#" class="hover:underline">Contact Us</a></li>
-                <li><a href="#" class="hover:underline">Subscribe</a></li>
-                <li><a href="#" class="hover:underline">Book an Appointment with Adoption Shelter</a></li>
-                <li><a href="#" class="hover:underline">Book an Appointment with Vet Clinic</a></li>
-              </ul>
-            </div>
+            <?php if ($quick_links_links) : ?>
+              <div class="w-1/3">
+                <?php if ($quick_links_heading) {
+                  echo '<h4 class="text-lg font-semibold mb-4">' . $quick_links_heading . '</h4>';
+                } ?>
+                <ul class="flex flex-col gap-y-3">
+                  <?php foreach ($quick_links_links as $link) {
+                    $link_url = $link['link']['url'] ?? '#';
+                    $link_target = $link['link']['target'] ?? '_self';
+                    $link_title = $link['link']['title'] ?? '';
+                    echo '<li><a href="' . $link_url . '" target="' . $link_target . '" class="hover:underline">' . $link_title . '</a></li>';
+                  } ?>
+                </ul>
+              </div>
+            <?php endif; ?>
+            <?php if ($about_us_links) : ?>
+              <div class="w-1/3">
+                <?php if ($about_us_heading) {
+                  echo '<h4 class="text-lg font-semibold mb-4">' . $about_us_heading . '</h4>';
+                } ?>
+                <ul class="flex flex-col gap-y-3">
+                  <?php foreach ($about_us_links as $link) {
+                    $link_url = $link['link']['url'] ?? '#';
+                    $link_target = $link['link']['target'] ?? '_self';
+                    $link_title = $link['link']['title'] ?? '';
+                    echo '<li><a href="' . $link_url . '" target="' . $link_target . '" class="hover:underline">' . $link_title . '</a></li>';
+                  } ?>
+                </ul>
+              </div>
+            <?php endif; ?>
+            <?php if ($get_in_touch_links) : ?>
+              <div class="w-1/3">
+                <?php if ($get_in_touch_heading) {
+                  echo '<h4 class="text-lg font-semibold mb-4">' . $get_in_touch_heading . '</h4>';
+                } ?>
+                <ul class="flex flex-col gap-y-3">
+                  <?php foreach ($get_in_touch_links as $link) {
+                    $link_url = $link['link']['url'] ?? '#';
+                    $link_target = $link['link']['target'] ?? '_self';
+                    $link_title = $link['link']['title'] ?? '';
+                    echo '<li><a href="' . $link_url . '" target="' . $link_target . '" class="hover:underline">' . $link_title . '</a></li>';
+                  } ?>
+                </ul>
+              </div>
+            <?php endif; ?>
           </div>
         </div>
       </div>
@@ -124,21 +202,33 @@ $section_id = 'section-subscribe-' . uniqid();
     <div class="container max-w-screen-2xl">
       <div class="flex gap-x-20">
         <div class="w-1/2">
-          <span class="text-sm">@2023 Cat Protection Society Victoria</span>
+          <?php if ($copyright_site_name) : ?>
+            <span class="text-sm"><?php echo $copyright_site_name ?></span>
+          <?php endif; ?>
         </div>
         <div class="w-1/2">
           <div class="flex justify-between">
-            <div class="flex gap-x-8 text-sm">
-              <a href="#" class="text-sm hover:underline">Terms and Conditions</a>
-              <a href="#" class="text-sm hover:underline">Privacy Policy</a>
-              <a href="#" class="text-sm hover:underline">Sitemap</a>
-            </div>
-            <ul class="social-link flex gap-x-1 px-6">
-              <li><a href="#" class="text-white font-medium hover:opacity-75"><?php echo cpsv_icon(array('icon' => 'facebook', 'group' => 'social', 'size' => '24', 'class' => 'w-6 h-6 text-brand-blue')); ?></a></li>
-              <li><a href="#" class="text-white font-medium hover:opacity-75"><?php echo cpsv_icon(array('icon' => 'instagram', 'group' => 'social', 'size' => '24', 'class' => 'w-6 h-6 text-brand-blue')); ?></a></li>
-              <li><a href="#" class="text-white font-medium hover:opacity-75"><?php echo cpsv_icon(array('icon' => 'tiktok', 'group' => 'social', 'size' => '24', 'class' => 'w-6 h-6 text-brand-blue')); ?></a></li>
-              <li><a href="#" class="text-white font-medium hover:opacity-75"><?php echo cpsv_icon(array('icon' => 'linkedin', 'group' => 'social', 'size' => '24', 'class' => 'w-6 h-6 text-brand-blue')); ?></a></li>
-            </ul>
+            <?php if ($copyright_links) : ?>
+              <div class="flex gap-x-8 text-sm">
+                <?php
+                foreach ($copyright_links as $link) :
+                ?>
+                  <a href="<?php echo $link['link']['url'] ?>" class="text-sm hover:underline"><?php echo $link['link']['title'] ?></a>
+                <?php endforeach ?>
+              </div>
+            <?php endif; ?>
+
+            <?php if ($social_links) : ?>
+              <ul class="social-link flex gap-x-1 px-6">
+                <?php
+                foreach ($social_links as $link) :
+                  $social_media_link = $link['social_media_link'] ?? '';
+                  $social_media_icon = $link['social_media_icon'] ?? '';
+                ?>
+                  <li><a href="<?php echo $social_media_link ?>" target="_blank" class="text-white font-medium hover:opacity-75"><?php echo cpsv_icon(array('icon' => $social_media_icon, 'group' => 'social', 'size' => '24', 'class' => 'w-6 h-6 text-brand-blue')); ?></a></li>
+                <?php endforeach ?>
+              </ul>
+            <?php endif; ?>
           </div>
         </div>
       </div>
