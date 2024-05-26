e4181447cf93234f475feca05903f917381dff4f
Slant Dev <build@amazeworx.com>
2024-05-27 03:57:10 +0700

    responsive

--- template-parts/global/separator.php
+++ template-parts/global/separator.php
@@ -8,9 +8,9 @@ $class = $args['class'] ?? '';
 $style = ($color) ? 'color: ' . $color . ';' : '';
 
 if ($location == 'top' && $active) :
-  echo '<div class="section-separator-top absolute h-12 w-px top-0 left-1/2 border-l border-solid border-slate-300" style="' . $style . '"></div>';
+  echo '<div class="section-separator-top absolute h-8 lg:h-12 w-px top-0 left-1/2 border-l border-solid border-slate-300" style="' . $style . '"></div>';
 endif;
 
 if ($location == 'bottom' && $active) :
-  echo '<div class="section-separator-bottom absolute h-12 w-px bottom-0 left-1/2 border-l border-solid border-slate-300" style="' . $style . '"></div>';
+  echo '<div class="section-separator-bottom absolute h-8 lg:h-12 w-px bottom-0 left-1/2 border-l border-solid border-slate-300" style="' . $style . '"></div>';
 endif;
