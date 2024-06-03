dbfabebfb087a34931a18faa29bed572fde8a2b3
Slant Dev <build@amazeworx.com>
2024-06-03 20:18:24 +0700

    woocommerce theme support

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -27,13 +27,16 @@ function cpsv_setup()
 	add_theme_support('custom-logo');
 	add_theme_support('post-thumbnails');
 
-	add_theme_support('align-wide');
-	add_theme_support('wp-block-styles');
-
 	add_theme_support('editor-styles');
 	add_editor_style('css/editor-style.css');
 
 	add_post_type_support('page', 'excerpt');
+
+	// Woocommerce
+	add_theme_support('woocommerce');
+	add_theme_support('wc-product-gallery-zoom');
+	add_theme_support('wc-product-gallery-lightbox');
+	add_theme_support('wc-product-gallery-slider');
 }
 
 add_action('after_setup_theme', 'cpsv_setup');
